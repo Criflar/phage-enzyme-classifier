0563750ffd293f85521cb389eb51b68df95b0550
Criflar <marvin_mangubat@dlsu.edu.ph>
2026-03-11 17:19:16 +0800

    Finished the data preprocessing, started with the BioPython analysis.

--- scripts.py
+++ scripts.py
@@ -1,1 +1,16 @@
-import numpy as np
+from Bio.SeqUtils.ProtParam import ProteinAnalysis
+import pandas as pd
+
+def extract_features(sequence):
+    """
+    Takes a single amino acid string.
+    Returns a pandas series containing Molecular Weight, pI, GRAVY, and Instability index)
+    """
+    analyzer = ProteinAnalysis(sequence)
+
+    weight = analyzer.molecular_weight()
+    pi = analyzer.isoelectric_point()
+    gravy = analyzer.gravy()
+    instability = analyzer.instability_index()
+
+    return pd.Series([weight, pi, gravy, instability])
